af4b2ca4d83cb31520cace0bdf62c35f098bb5e4
artificial-life <proteus.protocol@gmail.com>
2016-12-27 00:54:04 +0700

    upd

--- post_generator.js
+++ post_generator.js
@@ -3,51 +3,54 @@
 const uuid = require('uuid/v1');
 const _ = require('lodash');
 
-let template = function(text, timestamp, user_id) {
-  return {
-    id: uuid(),
-    text,
-    timestamp,
-    user_id
-  };
+let template = function(text, timestamp, user_id, group_id) {
+	return {
+		id: uuid(),
+		text,
+		timestamp,
+		user_id,
+		group_id
+	};
 };
 
 let letters = "abcdefghijklmnopqrstuvwxyz";
 
 let namegen = (len) => {
-  let result = "";
-  for (let i = 0; i < len; i++) {
-    let index = _.floor(Math.random() * (letters.length));
-    result += letters[index];
-  }
-  return result;
+	let result = "";
+	for (let i = 0; i < len; i++) {
+		let index = _.floor(Math.random() * (letters.length));
+		result += letters[index];
+	}
+	return result;
 };
 
 let gentext = () => {
 
-  let result = [];
+	let result = [];
 
-  for (var i = 0; i < 10; i++) {
-    let part = namegen(_.floor(Math.random() * 10));
-    result.push(part);
-  }
+	for (var i = 0; i < 10; i++) {
+		let part = namegen(_.floor(Math.random() * 10));
+		result.push(part);
+	}
 
-  return result.join(' ') + '!!!';
+	return result.join(' ') + '!!!';
 }
 
 let genuser = (i) => i % 2;
+let gengroup = (i) => i % 3;
 
 
 function* Gen(start_time = Date.now(), step = 10000, count = 1000) {
-  let time = start_time;
-  for (var i = 0; i < count; i++) {
-    let text = gentext();
-    let user = genuser(i);
+	let time = start_time;
+	for (var i = 0; i < count; i++) {
+		let text = gentext();
+		let user = genuser(i);
+		let group = gengroup(i);
 
-    yield new template(text, time, user);
+		yield new template(text, time, user, group);
 
-    time += _.floor(Math.random() * step);
-  }
+		time += _.floor(Math.random() * step);
+	}
 }
 
 module.exports = Gen;
